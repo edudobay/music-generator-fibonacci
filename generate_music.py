d43e33a37363a6644a394272e3c6fe1d28c2cad0
Eduardo Dobay <edudobay@gmail.com>
2018-04-08 23:53:59 -0300

    Reorganize code and give better names

--- generate_music.py
+++ generate_music.py
@@ -1,50 +1,65 @@
-import mingus.core.notes as notes
-from mingus.containers.Note import Note
-from mingus.containers.Track import Track
-from mingus.midi import MidiFileOut
 import numpy as np
 import sys
-
-def fib():
-    a, b = 1, 1
-    while True:
-        yield a
-        a, b = b, a + b
-
-def lengths(n, dist):
-    notelen, probs = np.array(dist).T
-    probs = (probs.astype(float) / probs.sum()).cumsum()
-    x = np.random.uniform(0, 1, (n,))
-    return notelen[probs.searchsorted(x)]
-
-dist = [
-    ( 1, 3),
-    ( 2, 4),
-    ( 4, 3),
-]
 
 def clamp(x, min, max):
     if x < min: return min
     if x > max: return max
     return x
 
-t = Track()
+def gen_fibonacci():
+    a, b = 1, 1
+    while True:
+        yield a
+        a, b = b, a + b
 
-def scramble(l):
+def random_lengths(amount, distribution):
+    notelen, probs = np.array(distribution).T
+    probs = (probs.astype(float) / probs.sum()).cumsum()
+    x = np.random.uniform(0, 1, (amount,))
+    return notelen[probs.searchsorted(x)]
+
+def random_permutation_of_range(l):
     x = np.arange(l)
     y = x.copy()
     np.random.shuffle(y)
     return dict(zip(x, y))
 
-note = 12
-s = scramble(9)
-for _, f, l in zip(range(1000), fib(), lengths(1000, dist)):
-    note, lastnote = (note + s[f % 9] - 4), note
-    note = int(clamp(note, 0, 24))
-    n = Note()
-    n.from_int(note + 36)
-    t.add_notes(n, l)
+def main():
+    import mingus.core.notes as notes
+    from mingus.containers.note import Note
+    from mingus.containers.track import Track
+    from mingus.midi import midi_file_out
 
-fname = sys.argv[1]
-MidiFileOut.write_Track(fname, t, bpm=84)
+    t = Track()
 
+    lengths_distribution = [
+        ( 1, 0.3),
+        ( 2, 0.4),
+        ( 4, 0.3),
+    ]
+
+    music_length = 1000
+
+    note_seed = 12
+
+    s = random_permutation_of_range(9)
+
+    def next_note(fib_seed, last_note):
+        n = last_note + s[fib_seed % 9] - 4
+        return int(clamp(n, 0, 24))
+
+    note = note_seed
+
+    for fib_seed, length in zip(gen_fibonacci(), random_lengths(1000, lengths_distribution)):
+
+        note = next_note(fib_seed, note)
+
+        n = Note()
+        n.from_int(note + 36)
+        t.add_notes(n, length)
+
+    fname = sys.argv[1]
+    midi_file_out.write_Track(fname, t, bpm=84)
+
+if __name__ == '__main__':
+    main()
